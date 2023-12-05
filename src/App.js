57772fab9177e843bb750c76ff72dc9985280c4d
Mars Y Huang <mars_y_huang@trendmicro.com>
2023-12-05 18:54:01 +0800

    Create initial map, with setup.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from "./logo.svg";
 import "./App.css";
 import NATIONAL_PARKS_DATA from "./data.json";
+import Map from "react-map-gl";
+import DeckGL, { GeoJsonLayer } from "deck.gl";
 
 const MAPBOX_ACCESS_TOKEN = "pk.eyJ1IjoiZmx5aW5nLW11c2siLCJhIjoiY2xwczdjemVuMDA5MzJpczB2MWI3cW9zbCJ9.z-ryCkd6XhC1602B554LYg";
 const MAP_STYLE = "https://basemaps.cartocdn.com/gl/positron-nolabels-gl-style/style.json";
 
+const INITIAL_VIEW_STATE = {
+  latitude: 39.8283,
+  longitude: -98.5795,
+  zoom: 3,
+  bearing: 0,
+  pitch: 30,
+};
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DeckGL initialViewState={INITIAL_VIEW_STATE} controller={true}>
+      <Map mapStyle={MAP_STYLE} mapboxAccessToken={MAPBOX_ACCESS_TOKEN} />
+    </DeckGL>
   );
 }
 
