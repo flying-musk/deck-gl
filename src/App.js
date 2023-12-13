44de790c0d80fde11d9e7c131e234a6248f4414d
rosethorn999 <24700793+rosethorn999@users.noreply.github.com>
2023-12-12 17:50:02 -0800

    fix: Hide bart on small zoom

--- src/App.js
+++ src/App.js
@@ -16,8 +16,46 @@ const INITIAL_VIEW_STATE = {
   bearing: 0,
   pitch: 0,
 };
-
+const MIN_DISPLAY_BART_ZOOM = 8;
 function App() {
+  const [nationalParksData, setNationalParksData] = useState(NATIONAL_PARKS_DATA);
+  const parkLayer = new GeoJsonLayer({
+    id: "nationalParks",
+    data: nationalParksData,
+    filled: true,
+    pointRadiusMinPixels: 2.5,
+    pointRadiusScale: 2000,
+    getPointRadius: (f) => 2.5,
+    getFillColor: (data) => (data.properties.searched ? [255, 165, 0, 250] : [86, 144, 58, 250]),
+    autoHighlight: true,
+    pickable: true,
+  });
+  const bartLayer = new GeoJsonLayer({
+    id: "GeoJsonLayer",
+    data: LINE_DATA,
+    extruded: true,
+    filled: true,
+    getElevation: 30,
+    getFillColor: [160, 160, 180, 200],
+    getLineColor: (f) => {
+      const hex = f.properties.color;
+      return hex
+        ? hex.match(/[0-9a-f]{2}/g).map((x) => parseInt(x, 16))
+        : [0, 0, 0];
+    },
+    getLineWidth: 20,
+    getPointRadius: 4,
+    getText: (f) => f.properties.name,
+    getTextSize: 12,
+    lineWidthMinPixels: 2,
+    pointRadiusUnits: "pixels",
+    pointType: "circle+text",
+    stroked: false,
+    textLineHeight: 2,
+    pickable: true,
+  });
+  const [layers, setLayers] = React.useState([]);
+  
   const handleInputChange = (e) => {
     const searchedKeyword = e.target.value;
 
@@ -39,44 +77,15 @@ function App() {
     });
   };
 
-  const [nationalParksData, setNationalParksData] = useState(NATIONAL_PARKS_DATA);
-
-  const layers = [
-    new GeoJsonLayer({
-      id: "nationalParks",
-      data: nationalParksData,
-      filled: true,
-      pointRadiusMinPixels: 2.5,
-      pointRadiusScale: 2000,
-      getPointRadius: (f) => 2.5,
-      getFillColor: (data) => (data.properties.searched ? [255, 165, 0, 250] : [86, 144, 58, 250]),
-      autoHighlight: true,
-      pickable: true,
-    }),
-    new GeoJsonLayer({
-      id: "GeoJsonLayer",
-      data: LINE_DATA,
-      extruded: true,
-      filled: true,
-      getElevation: 30,
-      getFillColor: [160, 160, 180, 200],
-      getLineColor: (f) => {
-        const hex = f.properties.color;
-        return hex ? hex.match(/[0-9a-f]{2}/g).map((x) => parseInt(x, 16)) : [0, 0, 0];
-      },
-      getLineWidth: 20,
-      getPointRadius: 4,
-      getText: (f) => f.properties.name,
-      getTextSize: 12,
-      lineWidthMinPixels: 2,
-      pointRadiusUnits: "pixels",
-      pointType: "circle+text",
-      stroked: false,
-      textLineHeight: 2,
-      pickable: true,
-    }),
-  ];
-
+  const handleViewStateChange = (e) => {
+    if (e.interactionState.isZooming) {
+      if (e.viewState.zoom > MIN_DISPLAY_BART_ZOOM) {
+        setLayers([parkLayer, bartLayer]);
+      } else {
+        setLayers([parkLayer]);
+      }
+    }
+  };
   return (
     <>
       <div className="absolute z-[100] top-[0] left-[0] right-[0] bg-[white] border-b p-[16px] pb-[12px] flex flex-col gap-[12px]">
@@ -100,9 +109,28 @@ function App() {
             </div>
           </div>
         </p>
-        <input className="outline-none grow self-stretch border rounded p-[6px] text-[14px]" type="text" onChange={handleInputChange} placeholder="Search for keywords like 'Park', etc." />
+        <input
+          className="outline-none grow self-stretch border rounded p-[6px] text-[14px]"
+          type="text"
+          onChange={handleInputChange}
+          placeholder="Search for keywords like 'Park', etc."
+        />
       </div>
-      <DeckGL initialViewState={INITIAL_VIEW_STATE} controller={true} layers={layers} getTooltip={({ object }) => object && (object.properties.Name ? `${object.properties.Name} (${object.properties.Code})` : object.properties.name || object.properties.station)}>
+      <DeckGL
+        initialViewState={INITIAL_VIEW_STATE}
+        controller={true}
+        onViewStateChange={handleViewStateChange}
+        onWebGLInitialized={() => {
+          setLayers([parkLayer, bartLayer]);
+        }}
+        layers={[layers]}
+        getTooltip={({ object }) =>
+          object &&
+          (object.properties.Name
+            ? `${object.properties.Name} (${object.properties.Code})`
+            : object.properties.name || object.properties.station)
+        }
+      >
         <Map mapStyle={MAP_STYLE} mapboxAccessToken={MAPBOX_ACCESS_TOKEN} />
       </DeckGL>
     </>
